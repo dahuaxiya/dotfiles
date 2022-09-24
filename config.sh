4b29c42ae84c1a726a958d7bfc7c52ad512ac64b
dahuaxiya <dahuaxiya@dhxy.com>
2022-09-24 13:49:45 +0800

    second

--- config.sh
+++ config.sh
@@ -3,9 +3,18 @@
 if [ ! -d ~/profile];then
 	mkdir ~/profile
 fi
-cp ~/.* ~/profile
-for filename in $(la ~/profile)
+#cp ~/.* ~/profile
+for dotfile in $(ls -da ~/.*)
 do
-	echo $filename
+	if [ -f ${dotfile} ] && [ ! -f ~/profile/${dotfile##*/} ];then
+		cp ${dotfile} ~/profile
+		[ -f ~/profile/${dotfile##*/} ]	echo "${dotfile} has been copied"
+	fi
+done
+
+for filename in $(ls -a ~/profile/.[^.]*)
+do
+	filename=${filename##*/}
 	ln -sf ~/profile/$filename ~/$filename
+	echo 'file' \"$filename\"  "has been linked"
 done
